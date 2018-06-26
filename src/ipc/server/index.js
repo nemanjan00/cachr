d80a2774bf7f1abb60383a1be6a97a78eb470cb2
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-06-26 22:57:06 +0200

    Add ipc server

--- src/ipc/server/index.js
+++ src/ipc/server/index.js
@@ -1,4 +1,25 @@
-module.exports = {
+const ipc = require("node-ipc");
 
+module.exports = (config) => {
+	const server = {
+		init: () => {
+			return new Promise((resolve) => {
+				ipc.config.id = "keshr";
+				ipc.config.retry= 1500;
+
+				ipc.serve(() => {
+					server.on = ipc.server.on;
+				});
+
+				ipc.server.start();
+
+				server.ipc = ipc;
+
+				resolve(server);
+			});
+		}
+	};
+
+	return server.init(config);
 };
 
